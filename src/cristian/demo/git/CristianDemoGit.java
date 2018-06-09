23edb8a648b2baaff79da0db74a6f6756cd941d8
cristian27dam <cristian.margar23@gmail.com>
2018-06-09 15:42:19 +0200

    Este es el mensaje que tendra el commit realizado.

---
+++ src/cristian/demo/git/CristianDemoGit.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cristian.demo.git;
+
+/**
+ *
+ * @author Kai
+ */
+public class CristianDemoGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Esto es una demo para el trabajo sobre Git....");
+    }
+    
+}
